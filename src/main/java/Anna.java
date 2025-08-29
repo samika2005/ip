0fe4b632b792ac489d7f94e6a2b52d1380eb4ed3
samika2005 <supersamika2005@gmail.com>
2025-08-30 06:26:56 +0800

    Level-0

---
+++ src/main/java/Anna.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Anna {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Hello! I'm Anna!");
+    System.out.println("What can i do for you?");
+    String placeholder = in.nextLine();
+    System.out.println("Bye. Hope to see you again soon!");
+    }
+}
