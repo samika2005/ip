1e6ae75967fab427bc6cebb18e2da305802d8226
samika2005 <supersamika2005@gmail.com>
2025-09-04 23:16:07 +0800

    level-4

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+ public class Task {
+    protected String word;
+    protected boolean flag;
+
+    public Task(String word) {
+        this.word = word;
+        this.flag = false;
+    }
+
+    public void mark(boolean done) {
+        this.flag = done;
+    }
+
+    @Override
+    public String toString() {
+        if (flag) {
+            return "[X] " + word;
+        } else {
+            return "[ ] " + word;
+        }
+    }
+}
